2551166f4c9f2489d21a77faf02320a4473341e8
Vivian Wei <vwei@smith.edu>
2022-09-22 00:26:51 -0400

    add string split

--- Conversation.java
+++ Conversation.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 
 class Conversation {
@@ -18,12 +19,14 @@ class Conversation {
     System.out.println("Hi there!");
     for(int i = 0; i<numRounds; i++){
       String userInput = input.next();
+      String[] splitted_userInput = userInput.split("\\s+");
       //create a random generator to select an array
       Random generator = new Random();
       int randomIndex = generator.nextInt(bot_response.length);
       String reply = bot_response[randomIndex];
+      System.out.println(Arrays.toString(splitted_userInput));
       System.out.println(reply);
-    }
+    };
     
     System.out.println("Thank you and bye!");
 
