d4144c067e9e0b1b13f74ce97355f4037ee28d68
Vivian Wei <vwei@smith.edu>
2022-09-21 23:30:33 -0400

    Print random reply

--- Conversation.java
+++ Conversation.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    //create a scanner that takes input from user
     Scanner input = new Scanner(System.in);
+    //prompt
     System.out.print("How many rounds? ");
     int numRounds = input.nextInt();
+    //create an array of response
+    String[] bot_response = {"Interesting", "Got it", "What do you mean?"
+    };
     // String userInput = "";
     //start rounds
     System.out.println("Hi there!");
     for(int i = 0; i<numRounds; i++){
       String userInput = input.next();
-        System.out.println(userInput);
+      //create a random generator to select an array
+      Random generator = new Random();
+      int randomIndex = generator.nextInt(bot_response.length);
+      String reply = bot_response[randomIndex];
+      System.out.println(reply);
     }
+    
     System.out.println("Thank you and bye!");
 
   }
